1bdfefbdd3f8fccd1094c670fa6d050afdbf2cfc
Rolin Blake <rolinmblake@gmail.com>
2024-01-18 11:02:06 -0600

    playing around

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,19 @@
 async function main() {
   console.log("served main.js successfully");
+  if (!navigator.gpu) {
+    alert("WebGPU NOT SUPPORTED\n(could be non-Chrome browser, could be on mobile, or could have no detectable GPU)");
+    return;
+  }
+  const adapter = await navigator.gpu.requestAdapter();
+  if (!adapter) {
+    alert("Couldn't request WebGPU adapter from navigator");
+    return;
+  }
+  const device = await adapter.requestDevice();
+  if (!device) {
+    alert("Couldn't get local GPU adapter logical device");
+    return;
+  }
 }
 
 window.addEventListener("load", main);
--- public/js/triangle.js
+++ public/js/triangle.js
@@ -1,14 +1,18 @@
 async function simpleTriangle() {
     if (!navigator.gpu) {
-      alert("WebGPU not supported");
+      alert("WebGPU NOT SUPPORTED\n(could be non-Chrome browser, could be on mobile, or could have no detectable GPU)");
       return;
     }
     const adapter = await navigator.gpu.requestAdapter();
     if (!adapter) {
-      alert("Couldn't request WebGPU adapter");
+      alert("Couldn't request WebGPU adapter from navigator");
       return;
     }
     const device = await adapter.requestDevice();
+    if (!device) {
+      alert("Couldn't get local GPU adapter logical device");
+      return;
+    }
     const shaders = `
     struct VertexOut{
       @builtin(position) position : vec4f,
