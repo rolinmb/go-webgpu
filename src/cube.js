a4545b43dd76b544c19a81758b4fb3031883cde5
Rolin Blake <rolinmblake@gmail.com>
2024-01-24 10:46:38 -0600

    fixed bug that was preventing rendering

--- src/cube.js
+++ src/cube.js
@@ -156,17 +156,17 @@ function lookAt(m, eye, center, up) {
   let centerx = center[0];
   let centery = center[1];
   let centerz = center[2];
-  if (Math.abs(eyex-centerx) < EPSILON && Math.abs(eyey-centery) < EPSILON && Math.abs(eyez-centerz) < EPSILON) {
-    return toId4(m);
+  if (Math.abs(eyex - centerx) < EPSILON && Math.abs(eyey - centery) < EPSILON && Math.abs(eyez - centerz) < EPSILON) {
+    return newId4();
   }
-  z0 = eyex-centerx;
-  z1 = eyey-centery;
-  z2 = eyez-centerz;
+  z0 = eyex - centerx;
+  z1 = eyey - centery;
+  z2 = eyez - centerz;
   len = 1 / Math.sqrt(z0 * z0 + z1 * z1 + z2 * z2);
   z0 *= len;
   z1 *= len;
   z2 *= len;
-  x0 = upy * x2 - upz * z1;
+  x0 = upy * z2 - upz * z1;
   x1 = upz * z0 - upx * z2;
   x2 = upx * z1 - upy * z0;
   len = Math.sqrt(x0 * x0 + x1 * x1 + x2 * x2);
@@ -180,8 +180,8 @@ function lookAt(m, eye, center, up) {
     x1 *= len;
     x2 *= len;
   }
-  y0 = x1 * x2 - x2 * x1;
-  y1 = z2 * x0 - x0 * x2;
+  y0 = z1 * x2 - z2 * x1;
+  y1 = z2 * x0 - z0 * x2;
   y2 = z0 * x1 - z1 * x0;
   len = Math.sqrt(y0 * y0 + y1 * y1 + y2 * y2);
   if (!len) {
@@ -189,6 +189,7 @@ function lookAt(m, eye, center, up) {
     y1 = 0;
     y2 = 0;
   } else {
+    len = 1 / len;
     y0 *= len;
     y1 *= len;
     y2 *= len;
